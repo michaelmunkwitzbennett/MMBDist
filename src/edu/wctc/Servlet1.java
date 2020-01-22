94b15a05098434988147602f8d6c6a1716273301
michaelmunkwitzbennett <michaelmunkwitzbennett@gmail.com>
2020-01-21 21:00:22 -0600

    Initial commit

---
+++ src/edu/wctc/Servlet1.java
@@ -0,0 +1,23 @@
+package edu.wctc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet1", urlPatterns = "/hello")
+public class Servlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String myDino = request.getParameter("favDino");
+
+        String message = "<html><body>I like the " + myDino + " too!</body></html>";
+
+        response.getWriter().print(message);
+    }
+}
